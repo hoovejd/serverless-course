cf5531fcbf7bf4e32c9203f4988f9f7ce32f3a2f
Jarrod Hoover <hoov85@gmail.com>
2024-07-17 06:47:42 -0400

    remove payload from test2

--- java_lambdas/invoke-lambda.sh
+++ java_lambdas/invoke-lambda.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# invoke lambda test1 passing event.json payload
 aws lambda invoke \
     --function-name java_lambda_test1 \
     --cli-binary-format raw-in-base64-out \
     --payload file://event.json out1.json
 
+# invoke lambda test2 with no payload
 aws lambda invoke \
     --function-name java_lambda_test2 \
     --cli-binary-format raw-in-base64-out \
-    --payload file://event.json out2.json
+    out2.json
