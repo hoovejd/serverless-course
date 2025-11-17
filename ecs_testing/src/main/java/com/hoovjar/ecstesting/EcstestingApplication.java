6902415109dc8d0514c3442974fa68e4fa2944bd
Jarrod Hoover <hoov85@gmail.com>
2025-11-17 16:39:11 -0500

    convert to command line runner app

--- ecs_testing/src/main/java/com/hoovjar/ecstesting/EcstestingApplication.java
+++ ecs_testing/src/main/java/com/hoovjar/ecstesting/EcstestingApplication.java
@@ -1,28 +1,25 @@
 package com.hoovjar.ecstesting;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.time.Instant;
 
 @Slf4j
 @SpringBootApplication
-@EnableScheduling
-public class EcstestingApplication {
+public class EcstestingApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcstestingApplication.class, args);
 	}
 
-	@Scheduled(fixedRateString = "${app.scheduler.fixedRate:60000}")
-	public void runScheduledTask() throws UnknownHostException {
-		log.info("Scheduled task executed at {} from IP address {}", Instant.now(), InetAddress.getLocalHost().getHostAddress());
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Command line task executed at {} from IP address {}", Instant.now(), InetAddress.getLocalHost().getHostAddress());
 	}
 
 }
